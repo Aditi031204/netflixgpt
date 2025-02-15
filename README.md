e9bdffdc59467f4660962aee78b4f34a03f04437
Aditi Singh Thakur <aditisingh.jbp@gmail.com>
2025-02-15 16:30:52 +0530

    message

---
+++ README.md
@@ -0,0 +1,3 @@
+# Netflixgpt
+- firstly we made a folder named netflixgpt
+- then by using vite framwork we installed tailwind css v4
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import React from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='bg-amber-600'>
+        hello world
+
+      </h1>
+    </div>
+  )
+}
+
+export default App
