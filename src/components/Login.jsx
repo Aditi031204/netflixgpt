baaa35f72d948395431ab86e1e5d15e4d6b343a2
Aditi Singh Thakur <aditisingh.jbp@gmail.com>
2025-02-18 23:53:41 +0530

    signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -25,18 +25,19 @@ const Login = () => {
   return (
     <div>
       <Header/>      
-      <div className="absolute">
-          <img className="bg-black bg-opacity-60" src= {IMG_URL} alt="logo" />       
+      <div className="absolute bg-black min-h-f min-w-fit z-20 box-border">
+          <img className="z-0" src= {IMG_URL} alt="logo" />
+          <div class="absolute inset-0 bg-black opacity-50"></div>       
       </div>
-      <form onSubmit={(e) => e.preventDefault()} className="w-4/12 h-12/12 absolute px-7 py-8 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-20">
+      <form onSubmit={(e) => e.preventDefault()} className="w-4/12 h-12/12 absolute px-7 py-8 bg-black my-36 z-30 mx-auto right-0 left-0 text-white opacity-80">
         <h1 className="font-bold text-[30px] m-2 ml-6">{isSignInForm ? "Sign In" : "Sign Up"}</h1>
         <input type="text" placeholder="Email or mobile number" className="my-2 p-4 mt-4 mb-3 ml-6 h-15 border border-gray-400 w-80 rounded-[5px]" ref={email}/>
         <input type="password" placeholder="Password" className="my-2 p-4 mt-1 mb-3 ml-6 h-15 border border-gray-400 w-80 rounded-[5px]" ref={password}/>
         <p className="text-red-600 ml-5 p-2">{errorMessage}</p>
-        <button className="my-2 p-2 ml-6 w-80 bg-red-700 rounded-[5px] font-semibold cursor-pointer" onClick={handelButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
+        <button className="my-2 p-2 ml-6 w-80 bg-red-600 hover:bg-red-700 opacity-100 rounded-[5px] font-semibold cursor-pointer text-white transition-all duration-300 ease-in-out shadow-lg" onClick={handelButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"}</button>
         <h1 className="text-gray-200 text-center my-1">OR</h1>
-        {isSignInForm && (<button className="my-2 p-2 ml-6 w-80 bg-gray-600 rounded-[5px] font-semibold cursor-pointer">Use a sign-in code</button>)}
-        <h1 className="text-gray-200 text-center my-1 cursor-pointer hover:underline hover:text-gray-400">Forgot password?</h1>
+        {isSignInForm && (<button className="my-2 p-2 ml-6 w-80 bg-[rgba(136,135,135,0.4)] hover:bg-[rgba(104,103,103,0.4)] rounded-[5px] font-semibold cursor-pointer text-white transition-all duration-300 ease-in-out shadow-lg">Use a sign-in code</button>)}
+        <h1 className="text-gray-200 text-center my-1 cursor-pointer hover:underline hover:text-[rgba(225,221,221,0.87)]">Forgot password?</h1>
         <label className="ml-6 mt-4 flex items-center gap-2.5">
           <input type="checkbox" className="scale-150 cursor-pointer" />
           Remember me
