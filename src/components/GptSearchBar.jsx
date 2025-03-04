236c8f0a4f00bda8fd4980de6c2e27b9bfd07d15
Aditi Singh Thakur <aditisingh.jbp@gmail.com>
2025-03-04 16:44:20 +0530

    Complete GPT Implement

--- src/components/GptSearchBar.jsx
+++ src/components/GptSearchBar.jsx
@@ -1,16 +1,28 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import React, { useRef } from "react";
 import lang from "../utils/LanguageConstant";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { options } from "../utils/constant";
+import { addGenMovieResults } from "../utils/GptSlice";
 
 const GptSearchBar = () => {
+  const dispatch = useDispatch();
   const langKey = useSelector((store) => store.config.lang);
   const searchText = useRef(null);
+
+  //Search movies in TMDB API
+  const searchMoviesTMDB = async (movies) => {
+    const data = await fetch("https://api.themoviedb.org/3/search/movie?query="+movies+"&include_adult=false&language=en-US&page=1", options);
+    const json = await data.json();
+    const OriginalMovie = json.results.filter((movie) => movie.title === movies);
+    // console.log(OriginalMovie)
+    return OriginalMovie;
+  };
 
   const handleGPTsearchClick = async () => {
     console.log(searchText.current.value);
     //Make an API call to get Movie Results
-    const gptQuery = "Act as a movie recomendation system and suggest some movies for the query" + searchText.current.value + ". Only give me names of 5 movies, comma separated like the example given ahead. Example Result: Bhool bhulaiya, Shaadi mai jaroor aana, Koi mill gaya, Kabhi khushi kabhi gum, Andhadhun."
+    const gptQuery = "Act as a movie recomendation system and suggest some movies for the query" + searchText.current.value + ". Only give me names of 5 movies, comma separated like the example given ahead. Example Result: Bhool bhulaiya,Shaadi mai jaroor aana,Koi mill gaya,Kabhi khushi kabhi gum,Andhadhun. Also give the results without any leading space"
     const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GENAI_API_KEY);
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
     const prompt = searchText.current.value;
@@ -21,6 +33,12 @@ const GptSearchBar = () => {
     }
     const GenMovies = result.response.text().split(",")
     console.log(GenMovies);
+
+    const PromiseArray = GenMovies.map((movies) => searchMoviesTMDB(movies));
+    const TmdbResults = await Promise.all(PromiseArray);       //Promise.all() take the array of promises
+    console.log(TmdbResults);
+
+    dispatch(addGenMovieResults({MovieNames: GenMovies, MovieResults: TmdbResults}));
   };
 
   return (
--- src/utils/GptSlice.jsx
+++ src/utils/GptSlice.jsx
@@ -4,13 +4,20 @@ const GptSlice = createSlice({
     name: "GPT",
     initialState: {
         ShowGPTsearch: false,
+        MovieNames: null,
+        MovieResults: null,
     },
     reducers: {
         toggleGPTsearchView: (state, action) => {
             state.ShowGPTsearch = !state.ShowGPTsearch;
-        }
+        },
+        addGenMovieResults: (state, action) => {
+            const {MovieNames , MovieResults} = action.payload;
+            state.MovieNames = MovieNames;
+            state.MovieResults = MovieResults;
+        },
     }
 });
 
-export const {toggleGPTsearchView} = GptSlice.actions;
+export const {toggleGPTsearchView, addGenMovieResults} = GptSlice.actions;
 export default GptSlice.reducer;
